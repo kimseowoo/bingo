748e51b1a376f1f17033023a9307592376936bea
kimseowoo <swoo422@naver.com>
2018-11-14 17:47:45 +0900

    03

--- main.c
+++ main.c
@@ -4,21 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-
 	int N;
 	int i,j;
-	int s[i][j];
+	int s[3][3]={
 	
-	N=5;
-	
-	for(i=0;i<N-1;i++)
+		{1, 2, 3},
+		{4, 5 ,6},
+		{7, 8, 9}};		//2차원 배열 초기화 
+
+
+	 N=5;
+	 		
+	for (i=0; i<N; i++)
 	{
-		for(j=0;j<N-1;j++)
-		{
-				printf("%d",s[i][j]=rand()%100);
-				printf("\n");
-		}
+		for(j=0; j<N; j++)
+			printf("%d ",s[i][j]);
+		printf("\n");
 	}
+	
 	
 	
 	return 0;
