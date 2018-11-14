acf00ab8a5fda6eb22d9de6b3f48add076832294
kimseowoo <swoo422@naver.com>
2018-11-14 18:58:16 +0900

    06

--- main.c
+++ main.c
@@ -10,16 +10,16 @@ int initate_bingo(void) {
 	
 		{1, 2, 3},
 		{4, 5 ,6},
-		{7, 8, 9}};		//2차원 배열 초기화 
+		{7, 8, 9}};		//이차원 배열 초기화 
 
 
 	return 0;
 }
 
-int main(void){
+int print_bingo(void){
 	
-	int N;
-	int s[N][N];
+	int N;			//빙고의 크기 
+	int s[N][N];	
 	int i,j;
 	
 	N=7;
@@ -29,11 +29,24 @@ int main(void){
 	for (i=0; i<N; i++)
 	{
 		for(j=0; j<N; j++)
-			printf("%d ",s[i][j]=rand()%max);
-		printf("\n");
+			printf("%d ",rand()%max);
+			printf("\n");
 	}
+	printf("\n");						
 	
+}//빙고 칸을 랜덤으로 채움 
+
+int main(void){
 	
-	}
+	print_bingo();
+	
+	int i;
+	
+	printf("숫자를 선택하세요 : ");
+	scanf("%d",&i);
+	printf("\n"); 
+	
+	printf("컴퓨터 숫자를 선택하세요 : %d",rand()%100);
+}
 	
 
