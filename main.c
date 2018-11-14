806ab0f91cb5419f6e39839fb8722ab69db11a0b
kimseowoo <swoo422@naver.com>
2018-11-14 17:36:32 +0900

    02

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 5
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	
-	int s[N][N];		//N*N배열 선언 
-	int i,j;
-	int max=N*N;
-	
-	srand((unsigned)time(NULL));
-	for(i=0; i<N; i++)
-		for(j=0; j<N; j++)
-			printf("%d", 1+rand()%max);
-	
 
+	int N;
+	int i,j;
+	int s[i][j];
 	
-		return 0;
+	N=5;
+	
+	for(i=0;i<N-1;i++)
+	{
+		for(j=0;j<N-1;j++)
+		{
+				printf("%d",s[i][j]=rand()%100);
+				printf("\n");
+		}
+	}
+	
+	
+	return 0;
 }
 	
 
