f1687aa09c0143558b4189a22c11cbb1b30e45c2
kimseowoo <swoo422@naver.com>
2018-11-17 18:38:43 +0900

    날라가서main2에다시만들었습니다.

---
+++ main2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int N;			//ºù°íÀÇ Å©±â 
+	int s[N][N];	
+	int i,j;
+
+	N=7;
+	
+	int max=N*N;
+	
+		
+	for (i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+			printf("%d ",1+rand()%max);
+			printf("\n");
+	
+	}
+	printf("\n");		
+	return 0;
+}
