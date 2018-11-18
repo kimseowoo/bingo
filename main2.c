acf7dfc542c9c2789223c0dfbbb30c331c2b8af7
kimseowoo <swoo422@naver.com>
2018-11-18 21:27:24 +0900

    1

--- main2.c
+++ main2.c
@@ -1,28 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 3
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(void){
-	
-	int i,j;
-	int s[N][N];
+ 
+void intate_bingo(int*bingo, int N)	
+{
+			
+	int i,j, temp;
 	int max=N*N;
-	
 	
 	for(i=0; i<N; i++)
 	{
 		for(j=0; j<N; j++)
+		{
+			temp = 1+rand()%max;
+		
+			if(same_num(bingo, temp, N))
+				j--;
+			else
+				bingo[i][j]=temp;
+		}
+	}
+}
+
+int same_num(int bingo, int temp, int N) 
+{
+	int i,j;
 	
-		printf("%d ",s[i][j]=1+rand()%max);
-		printf("\n");
-		
-		
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+		{
+			if(bingo[i][j]==temp)
+				return 1;
+		}
 	}
 	
 	return 0;
-	
 }
+
+int get_number_byMe()
+{	
+	int i;
+	
+	printf("숫자를 선택하세요 : ");
+	scanf("%d", &i);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
