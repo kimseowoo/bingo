a7d6148742b9655d2e45f7ac5882f1245af85553
kimseowoo <swoo422@naver.com>
2018-11-19 22:21:33 +0900

    7

--- main2.c
+++ main2.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 3
+#define N 7
 
  
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main()
 {
-	int s[N][N];
+	int s[N][N];	
 	
-	printf("<컴퓨터 빙고판>\n");
-	initate_bingo(s);
+	int ComBingo[N][N];
+	int UserBingo[N][N];
+	
+	printf("<ComBingo>\n");
+	initate_bingo(ComBingo);
 	printf("\n");
 	
-	
-	printf("<유저 빙고판>\n");
-	
-	initate_bingo(s);
-	
+	printf("<UserBingo>\n");
+	initate_bingo2(UserBingo);	
 }
 
 void initate_bingo(int s[N][N])	
@@ -64,20 +64,50 @@ void initate_bingo(int s[N][N])
 }
 
 
-
-	
-	
-void print_bingo(s)
+void initate_bingo2(int s[N][N])	
 {
-	int ComBingo[N][N];
-	int UserBingo[N][N];
-	
 	srand((unsigned int)time(NULL));
+
+	int i,j;
+	int max=N*N;
+	int count=1;
+	int x,y,n,m;				//맞교환할 배열 변수 
+	int temp; 			    	//맞교환할 수를 임시 보관 
 	
-	initate_bingo(ComBingo);
-	initate_bingo(UserBingo);
 	
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+		{
+			s[i][j] = count++;
+		}	
+	}									//1-N*N  이차원 배열 
+	
+	for(i=0; i<200; i++)
+	{
+		x=rand()%N;
+		y=rand()%N;
+		n=rand()%N;
+		m=rand()%N;
+		
+		temp=s[x][y];
+		s[x][y]=s[n][m];
+		s[n][m]=temp;
+	}									//200번 맞교환하여 랜덤 빙고판을 만듦 
+
+
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+		{		
+			printf("%d ",s[i][j]);
+		}
+			printf("\n");
+	}									//빙고판 출력 
 }
+	
+	
+
 	
 	
 	
