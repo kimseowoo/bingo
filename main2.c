4bd68d34dfdea44c773bf467f9af33d4bddd12e4
kimseowoo <swoo422@naver.com>
2018-11-19 21:46:17 +0900

    5

--- main2.c
+++ main2.c
@@ -9,8 +9,14 @@
 int main()
 {
 	int s[N][N];
+	int z[N][N];
 	
+	printf("<컴퓨터 빙고판>\n");
 	initate_bingo(s);
+	printf("\n");
+	
+	printf("<유저 빙고판>\n");
+	initate_bingo2(z);
 	
 }
 
@@ -55,7 +61,47 @@ void initate_bingo(int s[N][N])
 			printf("\n");
 	}									//빙고판 출력 
 }
+void initate_bingo2(int z[N][N])	
+{
+	srand((unsigned int)time(NULL));
 
+	int i,j;
+	int max=N*N;
+	int count=1;
+	int x,y,n,m;				//맞교환할 배열 변수 
+	int temp; 			    	//맞교환할 수를 임시 보관 
+	
+	
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+		{
+			z[i][j] = count++;
+		}	
+	}									//1-N*N  이차원 배열 
+	
+	for(i=0; i<100; i++)
+	{
+		x=rand()%N;
+		y=rand()%N;
+		n=rand()%N;
+		m=rand()%N;
+		
+		temp=z[x][y];
+		z[x][y]=z[n][m];
+		z[n][m]=temp;
+	}
+
+
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+		{		
+			printf("%d ",z[i][j]);
+		}
+			printf("\n");
+	}									//빙고판 출력 
+}
 
 
 
