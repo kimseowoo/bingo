65d9acda77e07ddd8756fe76fa881f1b3648b2da
kimseowoo <swoo422@naver.com>
2018-11-19 22:02:37 +0900

    5

--- main2.c
+++ main2.c
@@ -9,14 +9,15 @@
 int main()
 {
 	int s[N][N];
-	int z[N][N];
 	
 	printf("<컴퓨터 빙고판>\n");
 	initate_bingo(s);
 	printf("\n");
 	
+	
 	printf("<유저 빙고판>\n");
-	initate_bingo2(z);
+	
+	initate_bingo(s);
 	
 }
 
@@ -49,7 +50,7 @@ void initate_bingo(int s[N][N])
 		temp=s[x][y];
 		s[x][y]=s[n][m];
 		s[n][m]=temp;
-	}
+	}									//100번 맞교환하여 랜덤 빙고판을 만듦 
 
 
 	for(i=0; i<N; i++)
@@ -61,54 +62,22 @@ void initate_bingo(int s[N][N])
 			printf("\n");
 	}									//빙고판 출력 
 }
-void initate_bingo2(int z[N][N])	
+
+
+
+	
+	
+void print_bingo(s)
 {
+	int ComBingo[N][N];
+	int UserBingo[N][N];
+	
 	srand((unsigned int)time(NULL));
-
-	int i,j;
-	int max=N*N;
-	int count=1;
-	int x,y,n,m;				//맞교환할 배열 변수 
-	int temp; 			    	//맞교환할 수를 임시 보관 
 	
+	initate_bingo(ComBingo);
+	initate_bingo(UserBingo);
 	
-	for(i=0; i<N; i++)
-	{
-		for(j=0; j<N; j++)
-		{
-			z[i][j] = count++;
-		}	
-	}									//1-N*N  이차원 배열 
-	
-	for(i=0; i<100; i++)
-	{
-		x=rand()%N;
-		y=rand()%N;
-		n=rand()%N;
-		m=rand()%N;
-		
-		temp=z[x][y];
-		z[x][y]=z[n][m];
-		z[n][m]=temp;
-	}
-
-
-	for(i=0; i<N; i++)
-	{
-		for(j=0; j<N; j++)
-		{		
-			printf("%d ",z[i][j]);
-		}
-			printf("\n");
-	}									//빙고판 출력 
 }
-
-
-
-	
-	
-	
-	
 	
 	
 	
