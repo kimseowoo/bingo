d692188a77634c8783cfc590fcd21e29099d7639
kimseowoo <swoo422@naver.com>
2018-11-19 18:13:08 +0900

    2

--- main2.c
+++ main2.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-# 
+#include <time.h>
+#define N 5
+
  
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
- 
-void main()	
+
+int main()
 {
-	
-	int N;		
-	int i,j=0;
-	int max=N*N;
 	int s[N][N];
 	
-	N=5;
+	initate_bingo(s);
 	
-	srand(time(NULL));
-	
-	
-	for(i=0; i<N; i++)
-	{
-		for(j=0; j<N; j++)
-		
-			s[i][j] = 1+rand()%max;
-	}
-	
-	
-	for(i=0; i<N; i++)
-	{
-		for(j=0; j<N; j++)
-		{
-			printf("%d",s[i][j]);
-		}
-			printf("\n");
-	}
 }
 
+void initate_bingo(int s[N][N])	
+{
+	
+	long seed;
+	int i,j=0;
+	int temp[N*N];
+	int tem;
+		
+	seed = time(NULL);
+	srand(seed);
 	
 	
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+			s[i][j] = 1+rand()%N*N;
+	}							//·£´ýºù°íÆÇ »ý¼º 
 	
-	
-	
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+		{		
+			printf("%d ",s[i][j]);
+		}
+			printf("\n");
+	}									//ºù°íÆÇ Ãâ·Â 
+}
+
+
 	
 	
 	
